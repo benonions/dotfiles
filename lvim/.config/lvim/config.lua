04aea2970dd37548288c4a3da5f961a4c3000194
ben onions <benobions@bleep.bloop>
2023-04-02 09:29:52 +1000

    feat(lvim): add dap config, swapout tabnine-cmp for official

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,7 +1,7 @@
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
-lvim.colorscheme = "dracula"
+lvim.colorscheme = "tokyonight-moon"
 lvim.transparent_window = true
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -56,8 +56,83 @@ lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enable = true
 
 
+lvim.builtin.dap.active = true
+local dap = require("dap")
+
+dap.adapters.go = function(callback, _)
+  local stdout = vim.loop.new_pipe(false)
+  local handle
+  local pid_or_err
+  local port = 38697
+  local opts = {
+    stdio = { nil, stdout },
+    args = { "dap", "-l", "127.0.0.1:" .. port },
+    detached = true,
+  }
+  handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+    stdout:close()
+    handle:close()
+    if code ~= 0 then
+      print("dlv exited with code", code)
+    end
+  end)
+  assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+  stdout:read_start(function(err, chunk)
+    assert(not err, err)
+    if chunk then
+      vim.schedule(function()
+        require("dap.repl").append(chunk)
+      end)
+    end
+  end)
+  -- Wait for delve to start
+  vim.defer_fn(function()
+    callback { type = "server", host = "127.0.0.1", port = port }
+  end, 100)
+end
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+  {
+    type = "go",
+    name = "Debug",
+    request = "launch",
+    program = "${file}",
+  },
+  {
+    type = "go",
+    name = "Debug test", -- configuration for debugging test files
+    request = "launch",
+    mode = "test",
+    program = "${file}",
+  },
+  -- works with go.mod packages and sub packages
+  {
+    type = "go",
+    name = "Debug test (go.mod)",
+    request = "launch",
+    mode = "test",
+    program = "./${relativeFileDirname}",
+  },
+}
+
 -- Additional Plugins
 lvim.plugins = {
+  {
+    'codota/tabnine-nvim',
+    run = "./dl_binaries.sh",
+    require('tabnine').setup({
+      disable_auto_comment = true,
+      accept_keymap = "<Tab>",
+      dismiss_keymap = "<C-]>",
+      debounce_ms = 800,
+      suggestion_color = { gui = "#808080", cterm = 244 },
+      exclude_filetypes = { "TelescopePrompt" }
+    })
+  },
+  {
+    'ldelossa/gh.nvim',
+    requires = { { 'ldelossa/litee.nvim' } }
+  },
   {
     "Mofiqul/dracula.nvim"
   },
@@ -84,12 +159,12 @@ lvim.plugins = {
       require("octo").setup()
     end,
   },
-  {
-    "tzachar/cmp-tabnine",
-    run = "./install.sh",
-    requires = "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-  },
+  -- {
+  --   "tzachar/cmp-tabnine",
+  --   run = "./install.sh",
+  --   requires = "hrsh7th/nvim-cmp",
+  --   event = "InsertEnter",
+  -- },
   {
     "miversen33/netman.nvim",
     config = function()
