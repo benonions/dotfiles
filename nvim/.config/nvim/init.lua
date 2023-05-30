92657b77fa66c05188326ab820feeed244b039b0
Ben Onions <114976511+benonions@users.noreply.github.com>
2023-05-30 18:55:31 +1000

    nvim rewrite config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,14 +1,10 @@
--- Aesthetic
--- pcall catches errors if the plugin doesn't load
-local ok, tokyonight = pcall(require, "dracula")
-if not ok then return end
---vim.g.catppuccin_flavour = "frappe"
-tokyonight.setup()
-vim.cmd [[colorscheme dracula]]
+require "config.options"
+require "config.lazy"
 
-require('mr_onions.options')
-require('mr_onions.globals')
-require('mr_onions.lualine')
-require('mr_onions.keymap')
-require('mr_onions.lsp')
-require('mr_onions.telescope')
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    require("config.autocmds")
+    require("config.keymaps")
+  end
+})
