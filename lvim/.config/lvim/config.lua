c1640ea5bfbdc985eed1b92a828c7c72a0b06b47
Ben Onions <114976511+benonions@users.noreply.github.com>
2023-05-30 20:22:08 +1000

    lvim

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -2,10 +2,7 @@ lvim.format_on_save.enabled = true
 lvim.colorscheme = "tokyonight-moon"
 lvim.transparent_window = true
 lvim.relativenumber = true
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
 
--- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 vim.g.maplocalleader = ","
 
@@ -46,9 +43,7 @@ lvim.builtin.telescope.defaults.mappings = {
 }
 
 
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.alpha.active = true
+lvim.builtin.alpha.active = false
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
@@ -56,6 +51,7 @@ lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 lvim.builtin.bufferline.active = false
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
+  "go",
   "bash",
   "c",
   "javascript",
